6d2d3f089597523c9934009ebeeafb65ab7fb7ba
Hoon <sang.hoon.0905@gmail.com>
2020-10-10 09:36:56 +0900

    hi:

---
+++ Capston_Design/pre_processing.py
@@ -0,0 +1,8 @@
+from src.utils import linear_interpolation
+
+"""
+This file makes unprocessed csv file as a perfectly processed csv file for training
+The method of linear interpolation exist in the src\utils directories.
+
+"""
+# TODO
